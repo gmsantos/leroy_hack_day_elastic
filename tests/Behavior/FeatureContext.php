c3c485fcdfe1272fcd530a18709bb68c44b00665
Ricardo Plansky <ricardo.plansky@yahoo.com>
2017-03-17 16:00:55 -0300

    Created business readable tests

--- tests/Behavior/FeatureContext.php
+++ tests/Behavior/FeatureContext.php
@@ -12,4 +12,34 @@ class FeatureContext extends MinkContext implements
     Context,
     SnippetAcceptingContext
 {
+     /**
+     * @Then I should see DDrills logo
+     */
+    public function iShouldSeeDDrillsLogo()
+    {
+        $this->assertPageContainsText('DDrills');
+        $this->assertPageContainsText('The home of your holes');
+    }
+
+    /**
+     * @Then I should see the search bar
+     */
+    public function iShouldSeeTheSearchBar()
+    {
+        $this->assertElementOnPage('input[name=name]');
+        $this->assertElementOnPage('input[type=submit][value=Search]');
+    }
+
+    /**
+     * @Then I should see the sidebar
+     */
+    public function iShouldSeeTheSidebar()
+    {
+        $this->assertPageContainsText('Eletric_Voltage');
+        $this->assertPageContainsText('Line');
+        $this->assertPageContainsText('Indicated_Use');
+        $this->assertPageContainsText('Power');
+        $this->assertPageContainsText('Brand');
+        $this->assertPageContainsText('RPM');
+    }
 }
